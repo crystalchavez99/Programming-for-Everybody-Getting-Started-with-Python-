662eb23ca46d626c2afd048fbaff0a0a2d9cb742
Crystal <crystal120599@gmail.com>
2022-05-28 21:29:20 -0700

    week 4 done!

---
+++ week4/convert_input.py
@@ -0,0 +1,5 @@
+# Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+pay = float(hrs) * float(rate)
+print("Pay:",pay)
